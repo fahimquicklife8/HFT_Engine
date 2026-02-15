e19507d403088f943276658493dd5236702f1a24
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2026-02-15 18:56:10 -0500

    testing commit

---
+++ market_data/marketdata.cpp
@@ -0,0 +1,13 @@
+//
+// Created by fahim on 2/15/2026.
+//
+
+#include "marketdata.h"
+
+void marketdata() {
+
+    //api key from alpaca
+
+    int data = 0;
+    return;
+}
---
+++ market_data/marketdata.h
@@ -0,0 +1,16 @@
+//
+// Created by fahim on 2/15/2026.
+//
+
+#ifndef MARKETDATA_H
+#define MARKETDATA_H
+
+
+
+class marketdata {
+
+};
+
+
+
+#endif //MARKETDATA_H
